d641bb18510fba5c1a058bba598497ea5a2b3f7c
Evan C <88589282+EnviouslyEnvy@users.noreply.github.com>
2026-09-16 20:52:32 -0400

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        # Use a hashmap that corresponds a character to another TrieNode.
+        # Can use existence of these mappings to determine if next character exists.
+        self.endOfWord = False
+        # End of word will indicate that at that character the node ends 
+        
+        # insert will be like children["a"] = TrieNode()
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+        
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c] # shift pointer to appropriate child node.
+        return cur.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
